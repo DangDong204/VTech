6ed5462cca9b650c7c8feb5381082c7d755c0315
DangDong204 <165341409+DangDong204@users.noreply.github.com>
2026-03-26 20:43:17 +0700

    create UI dashboard, theme light & dark

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ProtectedRoute from '@/components/auth/ProtectedRoute'
+import DashboardHome from '@/pages/admin/dashboard/content/DashBoardHome'
+import LayoutAdmin from '@/pages/admin/LayoutAdmin'
+import SignUpPage from '@/pages/auth/LoginPage'
+import LoginPage from '@/pages/auth/SignUpPage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Toaster } from 'sonner'
-import SignUpPage from '@/pages/LoginPage'
-import LoginPage from '@/pages/SignUpPage'
 
 function App() {
   return (
@@ -20,16 +23,17 @@ function App() {
           {/* TODO: tạo các public route */}
           <Route path='/login' element={<LoginPage />} />
           <Route path='/signup' element={<SignUpPage />} />
+
           {/* TODO: tạo protected route */}
-          {/* <Route element={<ProtectedRoute />}> */}
-          {/* <Route path='/profile' element={<ProfilePage />} /> */}
-          {/* <Route path='/dashboard' element={<LayoutAdmin />}> */}
-          {/* <Route index element={<DashboardHome />} /> */}
-          {/* <Route path='users' element={<UserPage />} />
-              <Route path='categories' element={<CategoryPage />} />
+          <Route element={<ProtectedRoute />}>
+            {/* <Route path='/profile' element={<ProfilePage />} /> */}
+            <Route path='/dashboard' element={<LayoutAdmin />}>
+              <Route index element={<DashboardHome />} />
+              {/* <Route path='users' element={<UserPage />} /> */}
+              {/* <Route path='categories' element={<CategoryPage />} />
               <Route path='brands' element={<BrandPage />} /> */}
-          {/* </Route>
-          </Route> */}
+            </Route>
+          </Route>
           {/* <Route path='/' element={<TestPage />} /> */}
         </Routes>
       </BrowserRouter>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import '@/i18n/i18n.ts'
+import { ThemeProvider } from '@/components/provider/ThemeProvider.tsx'
+import { TooltipProvider } from '@/components/ui/tooltip'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TooltipProvider>
+      <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
+        <App />
+      </ThemeProvider>
+    </TooltipProvider>
   </StrictMode>
 )
